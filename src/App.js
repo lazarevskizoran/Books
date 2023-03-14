aacbdd097dca3411d78d09c45b6143138b723dfc
lazarevskizoran <100445665+lazarevskizoran@users.noreply.github.com>
2023-03-14 10:13:15 +0100

    New Code
    Update

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
+
+const App = () => {
+  const [books, setBooks] = useState([]);
+
+  const editBookById = (id, newTitle) => {
+    const updatedBooks = books.map((book) => {
+      if (book.id === id) {
+        return { ...books, title: newTitle };
+      }
+      return book;
+    });
+    setBooks(updatedBooks);
+  };
+
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+    setBooks(updatedBooks);
+  };
+
+  const createBook = (title) => {
+    const updatedBooks = [
+      ...books,
+      { id: Math.round(Math.random() * 9999), title },
+    ];
+    setBooks(updatedBooks);
+  };
+
+  return (
+    <div className='app'>
+      <BookList onEdit={editBookById} books={books} onDelete={deleteBookById} />
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/BookList.js
@@ -0,0 +1,11 @@
+import BookShow from './BookShow';
+const BookList = ({ books, onDelete, onEdit }) => {
+  const renderedBooks = books.map((book) => {
+    return (
+      <BookShow key={book.id} onEdit={onEdit} onDelete={onDelete} book={book} />
+    );
+  });
+  return <div className='book-list'>{renderedBooks}</div>;
+};
+
+export default BookList;
